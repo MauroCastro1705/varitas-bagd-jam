d124ab8a9f3544763623730a768d0eb25164d1c5
Juan Francisco Pintar <juanfranciscopintar@protonmail.com>
2026-09-12 01:19:56 -0300

    Fix books z ordering, rotation and font size

--- componentes/libro_prohibidos/libro_prohibido.gd
+++ componentes/libro_prohibidos/libro_prohibido.gd
@@ -1,9 +1,17 @@
 extends Control
-#libro prohibido
+## Libro prohibido
 
-@export var slide_length: int = 300  # Tiene que ser positivo
+@export_range(0, 1080, 10, "prefer_slider") var slide_length: int = 300
 @export var animation_duration: float = 0.3  # Duración de la animación en segundos
-#@onready var control_hover: Control = $shader TODO: Borrar si no hace nada
+
+# Reglas del libro
+@export var reglas1:String = "Velas de color negro
+Libro de las sombras
+Tierra de cementerio"
+@export var reglas2:String = "Huesos de gato
+Cabeza de gallina
+Muñeco vudú/ muñeco con alfileres
+Objetos con sangre"
 
 var initial_y: float
 var tween: Tween = null
@@ -11,23 +19,12 @@ var tween: Tween = null
 var button_small_size:Vector2 = Vector2(362.0, 434.0)
 var button_big_size:Vector2 = Vector2(711.0, 434.0)
 
-
 @onready var boton_movimiento: Button = %SlideButton
 @onready var libro_abierto: NinePatchRect = $Libro_abierto
 @onready var libro_cerrado: NinePatchRect = $Libro_cerrado
 
 @onready var pagina_1: Label = %pagina1
 @onready var pagina_2: Label = %pagina2
-
-#reglas del libro
-@export var reglas1:String = "Velas de color negro
-Libro de las sombras
-Tierra de cementerio"
-
-@export var reglas2:String = "Huesos de gato
-Cabeza de gallina
-Muñeco vudú/ muñeco con alfileres
-Objetos con sangre"
 
 
 func _ready() -> void:
@@ -44,18 +41,20 @@ func _ready() -> void:
 
 
 func _on_control_mouse_entered() -> void:
+	if get_parent() and boton_movimiento.button_pressed:
+		get_parent().move_child(self, -1)
 	if libro_abierto.material:
 		libro_abierto.material.set_shader_parameter("outline_width", 0.002)
-		#print("Hover activado")
 
 
 func _on_control_mouse_exited() -> void:
 	if libro_abierto.material:
 		libro_abierto.material.set_shader_parameter("outline_width", 0.0)
-		#print("Hover desactivado")
 
 
 func _on_slide_button_pressed(toggled_on: bool) -> void:
+	if get_parent():
+		get_parent().move_child(self, -1)
 	_slide(toggled_on)
 	if toggled_on:
 		libro_abierto.show()
@@ -71,11 +70,12 @@ func _slide(toggled_on: bool) -> void:
 	if tween and tween.is_running():
 		tween.kill()
 
-	tween = create_tween()
-	tween.set_ease(Tween.EASE_IN_OUT)
-	tween.set_trans(Tween.TRANS_CUBIC)
-
 	var target_pos = initial_y - slide_length if toggled_on else initial_y
+
+	tween = create_tween()\
+	.set_ease(Tween.EASE_IN_OUT)\
+	.set_trans(Tween.TRANS_CUBIC)
+
 	tween.tween_property(self, "position:y", target_pos, animation_duration)
 
 func achicar_boton():
