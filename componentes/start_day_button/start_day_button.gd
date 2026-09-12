33cae5e453f3136d24f6598a9c1d6a5a691072fe
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2026-09-12 11:21:58 -0300

    fix

--- componentes/start_day_button/start_day_button.gd
+++ componentes/start_day_button/start_day_button.gd
@@ -1,5 +1,5 @@
 extends Button
-@onready var nine_patch_rect: NinePatchRect = $NinePatchRect
+#@onready var nine_patch_rect: NinePatchRect = $NinePatchRect
 
 @onready var hover: NinePatchRect = $hover
 @onready var text_button: Label = $text
@@ -8,14 +8,14 @@ extends Button
 
 func _ready() -> void:
 	hover.hide()
-	nine_patch_rect.self_modulate = Color(0.663, 0.663, 0.663)	
+	#nine_patch_rect.self_modulate = Color(0.663, 0.663, 0.663)	
 
 
 func _on_mouse_entered() -> void:
 	hover.show()
-	nine_patch_rect.self_modulate = Color(1,1,1,1)
+	#nine_patch_rect.self_modulate = Color(1,1,1,1)
 	
 func _on_mouse_exited() -> void:
 	hover.hide()
-	nine_patch_rect.self_modulate = Color(0.663, 0.663, 0.663)
+	#nine_patch_rect.self_modulate = Color(0.663, 0.663, 0.663)
 	
